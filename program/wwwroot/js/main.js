28847233cef6fc8cf89fc9013fe2fc669069b088
alenSavov <playmatte@gmail.com>
2019-03-22 16:15:14 +0200

    Footer updated

--- program/wwwroot/js/main.js
+++ program/wwwroot/js/main.js
@@ -118,7 +118,11 @@ $(window).on('load', function() {
 			});
 		}
 
-	});
+    });
 
+    $("#githubLink").hover(
+        function () { $(this).animate({ width: "400px", height: "400px" }, 1000); },
+        function () { $(this).animate({ width: "200px", height: "200px" }, 1000); }
+    );
 })(jQuery);
 
